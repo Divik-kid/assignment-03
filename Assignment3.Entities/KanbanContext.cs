af1256de49b413790a61d9cd9d08ad2f4f9ba59e
GoodKidJonesy <80992821+GoodKidJonesy@users.noreply.github.com>
2022-09-28 20:03:18 +0200

    Make database

--- Assignment3.Entities/KanbanContext.cs
+++ Assignment3.Entities/KanbanContext.cs
@@ -2,12 +2,11 @@ namespace Assignment3.Entities;
 
 public class KanbanContext : DbContext
 {
-    public KanbanContext(){
 
-    }
-    
     public virtual DbSet<Task> Tasks => Set<Task>();
     public virtual DbSet<User> Users => Set<User>();
     public virtual DbSet<Tag> Tags => Set<Tag>();
 
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseNpgsql("Host=localhost;Database=assignment3db;Username=postgres;Password=abc");
 }
