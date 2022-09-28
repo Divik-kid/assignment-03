39a8b8010e54f216e2fdacdd41bee7b625ad9964
Chris <christopher.cortsen@gmail.com>
2022-09-28 22:53:30 +0200

    o_o

--- Assignment3.Entities.Tests/TagRepositoryTests.cs
+++ Assignment3.Entities.Tests/TagRepositoryTests.cs
@@ -1,14 +1,16 @@
 using Microsoft.Data.Sqlite;
+
 namespace Assignment3.Entities.Tests;
 
 using Assignment3;
 using Assignment3.Core;
 
-public sealed class TagRepositoryTests
+public sealed class TagRepositoryTests : IDisposable
 {
     private readonly KanbanContext _context;
     private readonly TagRepository _repository;
 
+    
     public TagRepositoryTests()
     {
         var connection = new SqliteConnection("Filename=:memory:");
@@ -17,25 +19,106 @@ public sealed class TagRepositoryTests
         builder.UseSqlite(connection);
         var context = new KanbanContext(builder.Options);
         context.Database.EnsureCreated();
-
-        
         context.SaveChanges();
+        var optionsBuilder = new DbContextOptionsBuilder<KanbanContext>();
+        _context = new KanbanContext(optionsBuilder.Options);
+        _repository = new TagRepository(_context);
+        _context.Database.BeginTransaction();
+    }
 
-        //_context = context;
-        //_repository = new TagRepository(_context);
+/*
+    [Fact]
+    public void Trying_to_delete_a_tag_in_use_without_the_force_should_return_Conflict()
+    {
+        // Given
+        var tag = new TagCreateDTO("My tag");
+        var res = _repository.Create(tag);
+
+        _context.Tags.Find(res.TagId)!.Tasks.Add(new Task());
+        _context.SaveChanges();
+        
+        // When
+        var response = _repository.Delete(res.TagId);
+    
+        // Then
+        response.Should().Be(Response.Conflict);
+    }
+*/
+    [Fact]
+    public void Tags_which_are_assigned_to_a_task_may_only_be_deleted_using_the_force()
+    {
+        // Given
+        var response = _repository.Delete(1, true);
+    
+        // When
+    
+        // Then
+        response.Should().Be(Response.Deleted);
+
+        // When
+        var deletedElement = _repository.Read(1);
+        deletedElement.Should().Be(null);
     }
 
     [Fact]
-    public void Create_given_City_returns_Created_with_City()
+    public void Trying_to_create_a_tag_which_exists_already_should_return_Conflict()
     {
-        var (response, created) = _repository.Create(new CityCreateDto("Central City"));
-
-        response.Should().Be(Created);
-
-        created.Should().Be(new CityDto(3, "Central City"));
+        // Given
+        var duplicateTag = new TagCreateDTO("Homework");
+    
+        // When
+        var duplicateResponse = _repository.Create(duplicateTag);
+    
+        // Then
+        duplicateResponse.Response.Should().Be(Response.Conflict);
     }
 
-        public void Dispose()
+    [Fact]
+    public void Creating_A_Nonexisting_Tag_Should_Return_Created()
+    {
+        // Given
+        var newTag = new TagCreateDTO("This tag has definitely not been created before!");
+
+        // When
+        var response = _repository.Create(newTag);
+    
+        // Then
+        response.Response.Should().Be(Response.Created);
+    }
+
+    [Fact]
+    public void Trying_to_update_a_non_existing_entity_should_return_NotFound()
+    {
+        // Given
+        var newTag = new TagUpdateDTO(1212, "This tag does not exist!");
+
+        // When
+        var response = _repository.Update(newTag);
+    
+        // Then
+        response.Should().Be(Response.NotFound);
+    }
+
+    [Fact]
+    public void Update_an_existing_tag_should_return_Updated()
+    {
+        // Given
+        var newTag = new TagUpdateDTO(1, "This is my new name");
+
+        // When
+        var response = _repository.Update(newTag);
+    
+        // Then
+        response.Should().Be(Response.Updated);
+
+        // When
+        var updatedTag = _repository.Read(1);
+
+        // Then
+        updatedTag.Name.Should().Be("This is my new name");
+    }
+
+    public void Dispose()
     {
         _context.Dispose();
     }
--- Assignment3.Entities/TagRepository.cs
+++ Assignment3.Entities/TagRepository.cs
@@ -1,5 +1,78 @@
 namespace Assignment3.Entities;
+using Microsoft.EntityFrameworkCore;
+using Assignment3.Core;
 
-public class TagRepository
+public sealed class TagRepository : ITagRepository
 {
+    private readonly KanbanContext _context;
+
+    public TagRepository(KanbanContext context) {
+        _context = context;
+    }
+
+    public (Response Response, int TagId) Create(TagCreateDTO tag)
+    {
+        var entity = _context.Tags.FirstOrDefault(t => t.Name == tag.Name);
+
+        Response response;
+
+        if (entity is null) {
+            entity = new Tag();
+            
+            _context.Tags.Add(entity);
+            _context.SaveChanges();
+
+            response = Response.Created;
+        } else {
+            response = Response.Conflict;
+        }
+
+        return (response, entity.Id);
+    }
+
+    public IReadOnlyCollection<TagDTO> ReadAll()
+    {
+        var collection = from t in _context.Tags
+                         select new TagDTO(t.Id, t.Name);
+                        
+        return collection.ToList();
+    }
+
+    public TagDTO Read(int tagId)
+    {
+        var entity = from t in _context.Tags
+                    where t.Id == tagId
+                    select new TagDTO(t.Id, t.Name);
+
+        return entity.FirstOrDefault()!;
+    }
+
+    public Response Update(TagUpdateDTO tag)
+    {
+        var entity = _context.Tags.FirstOrDefault(t => t.Id == tag.Id);
+        if (entity is null) return Response.NotFound;
+
+        entity.Name = tag.Name;
+        _context.SaveChanges();
+        return Response.Updated;
+    }
+
+    public Response Delete(int tagId, bool force = false)
+    {
+        var entity = _context.Tags.FirstOrDefault(t => t.Id == tagId);
+
+        if (entity is null) return Response.NotFound;
+
+        if (!force && entity.Tasks.Any())
+        {
+            return Response.Conflict;
+        } 
+
+        _context.Remove(entity);
+        _context.SaveChanges();
+        return Response.Deleted;
+    }
+
 }
+
+
