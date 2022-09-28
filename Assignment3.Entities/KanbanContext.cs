df26a51eff007694a43f533bcdca261e881a6db8
GoodKidJonesy <80992821+GoodKidJonesy@users.noreply.github.com>
2022-09-28 20:29:36 +0200

    db hotfix

--- Assignment3.Entities/KanbanContext.cs
+++ Assignment3.Entities/KanbanContext.cs
@@ -2,21 +2,11 @@ namespace Assignment3.Entities;
 
 public class KanbanContext : DbContext
 {
-    public KanbanContext(DbContextOptions<KanbanContext> options): base(options)
-    {}
+
     public virtual DbSet<Task> Tasks => Set<Task>();
     public virtual DbSet<User> Users => Set<User>();
     public virtual DbSet<Tag> Tags => Set<Tag>();
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder){
-
-        modelBuilder.Entity<Task>().Property(tsk => tsk.Title);
-        
-        modelBuilder.Entity<User>().Property(u => u.Name);
-        modelBuilder.Entity<User>().Property(u => u.Email);
-        modelBuilder.Entity<User>().HasIndex(u => u.Email).IsUnique();
-       
-        modelBuilder.Entity<Tag>().Property(tg => tg.Name);
-        modelBuilder.Entity<Tag>().HasIndex(tg => tg.Name).IsUnique();
-    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseNpgsql("Host=localhost;Database=assignment3db;Username=postgres;Password=abc");
 }
